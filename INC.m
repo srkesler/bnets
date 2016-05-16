31c842ce9c1fd315a79176ca4313cc8c24fc20e4
srkesler <srkesler@users.noreply.github.com>
2016-05-16 11:49:16 -0500

    Update INC.m

--- INC.m
+++ INC.m
@@ -15,62 +15,27 @@ close all
 if FilterIndex==0 error('File not selected'); end % Errors if the file is not selected
 NCdata=load(fullfile(root,FileName)); % Loads the data
 if length(fieldnames(NCdata))==1 NCdata=NCdata.(char(fieldnames(NCdata))); end
-ngroups=length(cell2mat(strfind(fieldnames(NCdata),'data')));
-ngroups=input('Enter the number of groups: ');
+ngroups=length(cell2mat(strfind(fieldnames(NCdata),'G')));
+    
+%% Loops across groups
 for igroup=1:ngroups
+    disp(sprintf('working on group%d',igroup)) % Displays the group the script is working on
     
-    ntp.(sprintf('group%d',igroup))=input(sprintf('Enter the number of time points for group%d: ',igroup));
-end
-%% Routine if there is only 1 timepoint for groups
-if ntp.group1==1
-    %% Loops across groups
-    for igroup=1:ngroups
-        disp(sprintf('working on group%d',igroup))
-        
-        %% Computing Correlation matrix with every sample in the group
-        loo.(sprintf('group%d',igroup)).data=NCdata.(sprintf('data%d',igroup));
-        loo.(sprintf('group%d',igroup)).groupname=NCdata.(sprintf('g%d',igroup));
-        loo.(sprintf('group%d',igroup)).SCN=corr(NCdata.(sprintf('data%d',igroup)));
-        
-        %% Computing Correlation matrix without one sample at a time in the group
-        for i = 1:size(NCdata.(sprintf('data%d',igroup)),1)
-            DI = NCdata.(sprintf('data%d',igroup)); DI(i,:) = [];
-            CI = corr(DI);
-            [loo.(sprintf('group%d',igroup)).mantelscore(i,1),loo.(sprintf('group%d',igroup)).mantelscorepval(i,1)] = bramila_mantel(CI,loo.(sprintf('group%d',igroup)).SCN,5000,'pearson');
-        end
-        %% Computing the contribution scores defined as 1-(mantel test score) and the average contribution in a group
-        loo.(sprintf('group%d',igroup)).contribution = 1-loo.(sprintf('group%d',igroup)).mantelscore(:,1);
-        loo.(sprintf('group%d',igroup)).avgcontribution=mean(loo.(sprintf('group%d',igroup)).contribution);
-    end
-    save(fullfile(root,'loomantelcontrib.mat'),'loo') % saves all the computed variables in a structure 'loo'
+    %% Computing Correlation matrix with every sample in the group
+    loo.(sprintf('group%d',igroup)).data=NCdata.(sprintf('G%d',igroup));%Storing Original data in the new structure
+    loo.(sprintf('group%d',igroup)).SCN=corr(NCdata.(sprintf('G%d',igroup))); % Auto Correlate data to find SCN with all subjects in a group
     
-    %% Routine if there are more than 1 timepoint for groups
-else
-    %% Loops across groups
-    for igroup=1:ngroups
-        for itp=1:ntp.(sprintf('group%d',igroup))
-            disp(sprintf('working on group%d timepoint%d',igroup,itp))
-            
-            %% Computing Correlation matrix with every sample in the group
-            loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).data=NCdata.(sprintf('data%dt%d',igroup,itp));
-            loo.(sprintf('group%d',igroup)).groupname=NCdata.(sprintf('g%d',igroup));%Storing group names in the new structure
-            loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).SCN=corr(NCdata.(sprintf('data%dt%d',igroup,itp))); 
-            
-            %% Computing Correlation matrix without one sample at a time in the group
-            for i = 1:size(NCdata.(sprintf('data%dt%d',igroup,itp)),1) 
-                DI = NCdata.(sprintf('data%dt%d',igroup,itp)); DI(i,:) = []; 
-                CI = corr(DI); %compute the correlation matrix without subject i
-                [loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).mantelscore(i,1),loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).mantelscorepval(i,1)] = bramila_mantel(CI,loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).SCN,5000,'pearson');
-            end
-            %% Computing the contribution scores defined as 1-(mantel test score) and the average contribution in a group
-            loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).contribution = 1-loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).mantelscore(:,1); %contribution score of 1 indicates highest contribution, 0 indicates lowest contribution
-            loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).avgcontribution=mean(loo.(sprintf('group%d',igroup)).(sprintf('tp%d',itp)).contribution);
-        end
-        
+    %% Computing Correlation matrix without one sample at a time in the group
+    for i = 1:size(NCdata.(sprintf('G%d',igroup)),1) % loops across subjects for Group1
+        DI = NCdata.(sprintf('G%d',igroup)); DI(i,:) = []; %create a data matrix without subject i
+        CI = corr(DI); %compute the correlation matrix without subject i
+        %Mantel test to get the statistics and pvalue between the correlation matrices with and without subject i
+        [loo.(sprintf('group%d',igroup)).mantelscore(i,1),loo.(sprintf('group%d',igroup)).mantelscorepval(i,1)] = bramila_mantel(CI,loo.(sprintf('group%d',igroup)).SCN,5000,'pearson');
     end
-    save(fullfile(root,'loomantelcontrib.mat'),'loo') % saves all the computed variables in a structure 'loo'
+    %% Computing the contribution scores defined as 1-(mantel test score) and the average contribution in a group
+    loo.(sprintf('group%d',igroup)).contribution = 1-loo.(sprintf('group%d',igroup)).mantelscore(:,1); %contribution score of 1 indicates highest contribution, 0 indicates lowest contribution
+    loo.(sprintf('group%d',igroup)).avgcontribution=mean(loo.(sprintf('group%d',igroup)).contribution);
 end
-
-
+save(fullfile(root,'loomantelcontrib.mat'),'loo') % saves all the computed variables in a structure 'loo'
 
 
