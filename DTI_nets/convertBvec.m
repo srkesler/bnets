7f27901e5211257650cfc90ed5621907693a54cf
srkesler <srkesler@users.noreply.github.com>
2016-08-26 18:02:40 -0500

    Create convertBvec.m

---
+++ DTI_nets/convertBvec.m
@@ -0,0 +1,15 @@
+% Shelli Kesler 8/20/16
+% Convert FSL bvec files to Diffusion Toolkit format
+
+[dirs] = textread('/Users/skesler/Desktop/tracks/Paths.txt' , '%s');
+% Txt file contains full paths to each subj e.g. /Volumes/STUDY/SUB001
+
+for i = 1:length(dirs)
+    cd(dirs{i});
+    %5 = no. chars from end of the subj path that represent subj ID
+    %starting count from 0, i.e. this selects the last 6 chars:
+    bvec = importdata([dirs{i}(end-5:end) '_bvecs'],' ');  
+    fileID = fopen('bvec.txt','w'); %create text file
+    %format data in 3 columns separated by 1 space and a comma with 4 decimals
+    fprintf(fileID,'%1.4f, %1.4f, %1.4f\n',bvec); 
+end
